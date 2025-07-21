cc554c252e9681fb40730a250374cc6ffde06d33
phamngocanit82 <ngocanphamit271082@gmail.com>
2025-07-21 00:44:34 -0500

    update

---
+++ Assets/Brick/Scripts/BrickResetZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class BrickResetZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        BrickGameManager.Instance.OnBallMiss();
+    }
+
+}
